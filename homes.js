72427efa0fa0eab2379ec5d4d163033fa82e8f39
false0a <4false@gmail.com>
2023-01-08 22:03:49 +0900

    두개의 디데이 만듬

--- homes.js
+++ homes.js
@@ -1,16 +1,33 @@
 const interval = setInterval(function() {
-    const deadline = new Date("January 1, 2021 00:00:00"); // 디데이가 주어진 시간
+    const deadline1 = new Date("May 13, 2021 00:00:00"); // 첫 번째 디데이가 주어진 시간
+    const deadline2 = new Date("May 14, 2021 00:00:00"); // 두 번째 디데이가 주어진 시간
     const currentTime = new Date(); // 현재 시간
-    const difference = deadline - currentTime; // 현재 시간과 디데이 시간의 차이
+    let difference1 = deadline1 - currentTime; // 첫 번째 디데이의 현재 시간과 차이
+    let difference2 = deadline2 - currentTime; // 두 번째 디데이의 현재 시간과 차이
   
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24)); // 일 단위
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // 시간 단위
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)); // 분 단위
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000); //
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-    
-    
-    }, 1000); // 1초마다 카운트다운 진행
+  // 첫 번째 디데이의 일, 시, 분, 초 계산
+  const days1 = Math.floor(difference1 / (1000 * 60 * 60 * 24));
+  const hours1 = Math.floor((difference1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes1 = Math.floor((difference1 % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds1 = Math.floor((difference1 % (1000 * 60)) / 1000);
+  
+  // 두 번째 디데이의 일, 시, 분, 초 계산
+  const days2 = Math.floor(difference2 / (1000 * 60 * 60 * 24));
+  const hours2 = Math.floor((difference2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes2 = Math.floor((difference2 % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds2 = Math.floor((difference2 % (1000 * 60)) / 1000);
+
+  if (difference1 < 0) {
+    document.getElementById("days1").innerHTML = 'ㄴ' + '+' + Math.abs(days1) + ' ' + ('0' + Math.abs(hours1)).slice(-2) + ':' + ('0' + Math.abs(minutes1)).slice(-2) + ':' + ('0' + Math.abs(seconds1)).slice(-2);
+  } else {
+    document.getElementById("days1").innerHTML = days1 + ' ' + ('0' + hours1).slice(-2) + ':' + ('0' + minutes1).slice(-2) + ':' + ('0' + seconds1).slice(-2);
+  }
+  
+  if (difference2 < 0) {
+    document.getElementById("days2").innerHTML = 'ㄴ' + '+' + Math.abs(days2) + ' ' + ('0' + Math.abs(hours2)).slice(-2) + ':' + ('0' + Math.abs(minutes2)).slice(-2) + ':' + ('0' + Math.abs(seconds2)).slice(-2);
+  } else {
+    document.getElementById("days2").innerHTML = days2 + ' ' + ('0' + hours2).slice(-2) + ':' + ('0' + minutes2).slice(-2) + ':' + ('0' + seconds2).slice(-2);
+  }
+  
+
+})
